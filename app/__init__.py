7cd5f94abfca118d388eef89d579d5f992c8137a
SavorTheFlavor2 <2500364530@qq.com>
2018-02-03 23:02:50 +0800

    flask_sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
-from app.admin import admin as admin_blueprint
-from app.home import home as home_blueprint
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:23456@localhost:3306/movie'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+from app.admin import admin as admin_blueprint
+from app.home import home as home_blueprint
 app.register_blueprint(admin_blueprint, url_prefix="/admin" )
 app.register_blueprint(home_blueprint)
