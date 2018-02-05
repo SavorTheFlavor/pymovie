acdf68a4f583c5162c87daa1772d49f897807631
SavorTheFlavor2 <2500364530@qq.com>
2018-02-05 21:31:29 +0800

    add some routes and corresponding templates

--- app/home/views.py
+++ app/home/views.py
@@ -1,7 +1,43 @@
 from . import home  # import home blueprint
+from flask import url_for, render_template, redirect
 
 
 @home.route("/")
 def index():
-    return "<h1 style='color: green;'>this is home!</h1>"
+    return render_template("home/index.html")
+
+
+@home.route("/login")
+def login():
+    return render_template("home/login.html")
+
+
+@home.route("/logout")
+def logout():
+    return redirect(url_for("home.login"))
+
+
+@home.route("/register")
+def register():
+    return render_template("home/register.html")
+
+@home.route("/user")
+def user():
+    return render_template("home/user.html")
+
+@home.route("/psw")
+def psw():
+    return render_template("home/psw.html")
+
+@home.route("/comments")
+def comments():
+    return render_template("home/comments.html")
+
+@home.route("/loginlog")
+def loginlog():
+    return render_template("home/loginlog.html")
+
+@home.route("/moviecol")
+def moviecol():
+    return render_template("home/moviecol.html")
 
